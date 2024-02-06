5c2b78bd6bb586b1571db0547e63a5db2c0e8faa
githigaurav <gfxshaolin@gmail.com>
2024-02-06 12:52:00 +0530

    button min-w fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const About = () => {
+
+   
+  
     return (
         <>
             <div className="w-full  flex  items-center justify-center mt-10 mb-10">
@@ -23,10 +26,11 @@ const About = () => {
                             that matches my skills and experience then don't hesitate to
                             contact me.
                         </p>
-
-                        <button className="bg-ctaColor text-white py-1 lg:text-xl max-w-[150px] lg:max-w-[250px]">
+                        <a href="#contact" className="block w-full">
+                        <button className="bg-ctaColor text-white py-1 lg:text-xl min-w-[150px]  max-w-[150px]  lg:max-w-[250px]">
                             Contact
                         </button>
+                        </a>
                     </div>
 
                     <div className="flex-1 flex flex-col gap-2 lg:gap-5">
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
             <div className='flex flex-col items-center p-1 gap-4 w-full max-w-[1440px]  '>
                 <h1 className=' uppercase font-bold text-ctaColor text-5xl lg:text-7xl 2xl:text-[130px]'><span className='text-textColor'>Hey, I'm </span> Gaurav</h1>
                 <p className='text-textColor font-regular text-center 2xl:text-[20px] max-w-[550px]  2  xl:max-w-[768px]'>A MERN STACK Web Developer building the Frontend & Backend of Websites and Web Applications that leads to the success of the overall product</p>
-                <a href="#projects" className=' w-full block text-center ' > <button className='bg-ctaColor text-white w-full max-w-[200px] xl:max-w-[250px] py-2 rounded-md mt-5 uppercase hover:bg-ctaHover transition-all'>projects</button></a>
+                <a href="#projects" className='' > <button className='bg-ctaColor text-white w-full min-w-[200px] max-w-[200px] xl:max-w-[250px] py-2 rounded-md mt-5 uppercase hover:bg-ctaHover transition-all'>projects</button></a>
             </div>
         </main>
 
