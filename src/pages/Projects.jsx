69297da673ac9731313a5dc2dc3f1d392962814f
githigaurav <gfxshaolin@gmail.com>
2024-02-07 15:21:17 +0530

    link added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,16 +1,11 @@
 import React, { useState } from 'react'
-import CaseStudy from './CaseStudy'
+
 
 const Projects = () => {
-  const [model, setModel]=useState(false)
 
-  const handleModel = () =>{   
-    setModel(false)
-   
-  }
   return (
     <>
-    {model && <CaseStudy handleModel={handleModel}/>}
+
         <div className='flex items-center flex-col gap-10 mb-10 mt-10'>
 
         <div className='flex  md:flex-row max-w-[1120px] flex-col p-2 w-full  gap-5 items-center'>
@@ -23,7 +18,7 @@ const Projects = () => {
                 <div className=' flex flex-1 flex-col items-start gap-5 '>
                     <h3 className=' text-2xl  font-bold text-ctaColor'>Responsive Portfolio</h3>
                     <p className='text-textColor  text-[18px]'>My portfolio represents the culmination of my experiences and skills as a MERN Stack Developer. Through this platform, I invite you to explore my projects, delvelop into my technical expertise, and witness my passion for creating exceptional web applications. </p>
-                    <button className='bg-ctaColor px-10 py-2 text-white upp' onClick={()=>{setModel(true)}}>Case Study</button>
+                    <button className='bg-ctaColor px-10 py-2 text-white upp max-w-[200px] w-full' onClick={()=>{window.open("https://beta.higaurav.com", "blank")}}>Visit</button>
                 </div>
         </div>
 
@@ -38,7 +33,7 @@ const Projects = () => {
                     <h3 className=' text-2xl  font-bold text-ctaColor'>Currency Converter</h3>
                     <p className='text-textColor  text-[18px]'> Instantly view currency rates with our React Currency Converter. Simply input numbers to see
                     real-time exchange rates for different countries, streamlining your currency comparisons. </p>
-                    <button className='bg-ctaColor px-10 py-2 text-white upp'>Case Study</button>
+                    <button className='bg-ctaColor px-10 py-2 text-white upp max-w-[200px] w-full' onClick={()=>{window.open("https://currencyconverter-gold.vercel.app", "blank")}} >Visit</button>
                 </div>
         </div>
       
