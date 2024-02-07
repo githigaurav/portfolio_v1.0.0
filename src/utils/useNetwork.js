d2ba66578c6e57abd4f76bd044d152672354bf88
githigaurav <gfxshaolin@gmail.com>
2024-02-07 09:15:04 +0530

    network call updated

--- src/utils/useNetwork.js
+++ src/utils/useNetwork.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const URL= window.location.hostname === "localhost" ? "http://localhost:3001" : "https://server-m6jlrtkzu-githigauravs-projects.vercel.app"
+const URL= window.location.hostname === "localhost" ? "http://localhost:3001" : "https://server-8bt8.onrender.com"
 
 export const useNetwork =async(method="post", URI="", payload={}, dataType={"Content-Type":"application/json"})=>{   
     const response = await axios[method](`${URL}/${URI}`, payload , dataType)
