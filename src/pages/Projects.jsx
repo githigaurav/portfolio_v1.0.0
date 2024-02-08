d35dfb5917c87f51abe6932e8bd6bf5806e3bf72
githigaurav <gfxshaolin@gmail.com>
2024-02-08 20:01:41 +0530

    sellmycasa project added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -37,6 +37,21 @@ const Projects = () => {
                 </div>
         </div>
       
+        <div className='flex  md:flex-row max-w-[1120px] flex-col p-2 w-full  gap-5 items-center'>
+                <div className='flex-1 relative -z-10 '>
+                  <img src={"/sellmycasa.png"} alt="" className='w-full h-auto max-w-[500px] ' />
+                  <div className='absolute bottom-0 left-5 '>
+                    <img src={"/sellmycasaMob.png"} alt="" className='max-h-[150px] sm:max-h-[200px]' />
+                  </div>
+                </div>
+                <div className=' flex flex-1 flex-col items-start gap-5 '>
+                    <h3 className=' text-2xl  font-bold text-ctaColor'>Sell My Casa</h3>
+                    <p className='text-textColor  text-[18px]'> SellMyCasa Your hassle-free solution to selling property online. List your home effortlessly and connect with motivated buyers, all through our user-friendly platform. Get started today and streamline your selling process with SellMyCasa.
+                     </p>
+                    <button className='bg-ctaColor px-10 py-2 text-white upp max-w-[200px] w-full' onClick={()=>{window.open("https://sellmycasa.vercel.app/", "blank")}}>Visit</button>
+                </div>
+        </div>
+
         </div>
 
     </>
